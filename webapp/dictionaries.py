aac7b7db9ba5e951342419cbc64f83b384331d86
cesarRz <cesarrz2000@gmail.com>
2021-12-06 15:23:31 -0600

    ID's en universal

--- webapp/dictionaries.py
+++ webapp/dictionaries.py
@@ -19,24 +19,26 @@ def TableValues(elemento):
 def universal(equipo):
     if equipo == "farinografo":
         class unidades():
-              absorcion_agua= '51.6–63.4 ml/100g'
-              tolerancia_ub= '20 - 28 '
-              elasticidad= ''
-              viscodidad= ''
-              act_enzimatica= ''
-              trigo_germinado= '60 - 75'
-              tiempo_amasado= ''
-              cantidad_gluten= ''
-              calidad_gluten= ''
-              indoneidad= ''
-              dureza= '54-145 FU'
-              reblandecimiento= ' '
-              estabilidad= '4.45–11.20 min'
-              tiempo_desarrollo= '8.3 - 9.3 min'
-              qnumber= '24 - 125'
+            id_far = "Universal"
+            absorcion_agua= '51.6–63.4 ml/100g'
+            tolerancia_ub= '20 - 28 '
+            elasticidad= ''
+            viscodidad= ''
+            act_enzimatica= ''
+            trigo_germinado= '60 - 75'
+            tiempo_amasado= ''
+            cantidad_gluten= ''
+            calidad_gluten= ''
+            indoneidad= ''
+            dureza= '54-145 FU'
+            reblandecimiento= ' '
+            estabilidad= '4.45–11.20 min'
+            tiempo_desarrollo= '8.3 - 9.3 min'
+            qnumber= '24 - 125'
                 
     if equipo == "alveografo":
         class unidades():
+            id_var = "Universal"
             tenacidad= '45'
             extensibilidad= '71.96'
             fuerza_panadera= '60'
